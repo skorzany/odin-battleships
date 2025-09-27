be3584dfec245b83d62016b48cb182be0d0338f7
skorzany <177423232+skorzany@users.noreply.github.com>
2025-09-27 14:20:39 +0200

    Add jest, ESLint, prettier & configure those

---
+++ .eslintrc.js
@@ -0,0 +1,28 @@
+module.exports = {
+  env: {
+    browser: true,
+    es2021: true,
+    jest: true,
+  },
+  extends: ['airbnb-base', 'prettier'],
+  plugins: ['prettier'],
+  overrides: [
+    {
+      env: {
+        node: true,
+      },
+      files: ['.eslintrc.{js,cjs}'],
+      parserOptions: {
+        sourceType: 'script',
+      },
+    },
+  ],
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
+  },
+  rules: {
+    'prettier/prettier': ['error'],
+    'no-underscore-dangle': ['off'],
+  },
+};
