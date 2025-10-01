f28086867b40d45c7db00d80c2a1f93161ae53d7
skorzany <177423232+skorzany@users.noreply.github.com>
2025-10-01 17:47:51 +0200

    Add GameBoard class skeleton & begin working on tests

---
+++ src/gameboard.js
@@ -0,0 +1,9 @@
+export default class Gameboard {
+  constructor() {
+    this.board = new Array(10).fill(new Array(10).fill(null));
+  }
+
+  placeShip() {}
+  receiveAttack() {}
+  hasShips() {}
+}
